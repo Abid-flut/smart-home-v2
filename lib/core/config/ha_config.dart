d2dcad26b9e02b6fb8465d4f0988eddc88aad8e6
Abid-flut <abidahmeddddd@gmail.com>
2026-03-24 15:25:56 +0200

    Remove sensitive config file

--- lib/core/config/ha_config.dart
+++
@@ -1,4 +0,0 @@
-class HaConfig {
-  static const String baseUrl = "https://vsr3pn54pqbbr9y00m9t4wxxjx1l2vzb.ui.nabu.casa";
-  static const String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIzYjEzOGIyZDUyYWY0NGEyOTBmMWYzZWJmNGNkYmFiYiIsImlhdCI6MTc3NDM0OTEwOSwiZXhwIjoyMDg5NzA5MTA5fQ.DoC9aZ69InNf34XuGO3WVLpeGhN9g0RkqnDT3yDt6w8";
-}
